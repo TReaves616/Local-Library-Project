5f41fb7b02ca5ebc1a05aa12ad55a16a0a9f5ae4
TReaves616 <81585180+TReaves616@users.noreply.github.com>
2021-08-04 19:31:56 -0400

    Update accounts.js

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,9 +1,13 @@
+//Finds the accounts by the given ID
 function findAccountById(accounts, id) {
+  //Uses find.
   return accounts.find((account) => account.id.includes(id));
 }
 
+//Sorts the accounts by last name
 function sortAccountsByLastName(accounts) {
-  return accounts.sort((lastA, lastB) =>
+  //Uses sort
+  return accounts.sort((lastA,lastB) => 
     lastA.name.last.toLowerCase() > lastB.name.last.toLowerCase() ? 1 : -1
   );
 }
@@ -11,7 +15,6 @@ function sortAccountsByLastName(accounts) {
 function getTotalNumberOfBorrows(account, books) {
   let result = 0;
   const booksBorrowedByAccount = books.forEach((book) => {
-    //loops through the borrows array to check if the accounts ID matches
     if (!!book.borrows) {
       book.borrows.forEach((accounts) => {
         if (accounts.id === account.id) {
@@ -23,31 +26,28 @@ function getTotalNumberOfBorrows(account, books) {
   return result;
 }
 
+//Helper function
+//Returns author object
+function _getAuthor(book, authors) {
+  const author = authors.find((author) => author.id === book.authorId);
+  return author;
+}
+
 function getBooksPossessedByAccount(account, books, authors) {
-  //Creates empty array to be filled with borrowed books
-    const borrowedBooks = [];
-    books.forEach((book) => {
-      let bookBorrows = book.borrows;
-  //loops through the borrows array to check if the borrowers ID matches the accounts ID
-  //with the condition that the book has not been returned
-      bookBorrows.forEach((borrow) => {
-        if (borrow.id === account.id && !borrow.returned) {
-          borrowedBooks.push(book);
-        }
-      });
+  const borrowedBooks = [];
+  books.forEach((book) => {
+    let bookBorrows = book.borrows;
+    bookBorrows.forEach((borrow) => {
+      if (borrow.id === account.id && !borrow.returned) {
+        borrowedBooks.push(book);
+      }
     });
-    //returns a new object within an array of all of the borrowed books
-    let result = borrowedBooks.map((book) => {
-      return { ...book, author: getAuthor(book, authors) };
-    });
-    return result;
-  }
-  // Helper function
-  // Returns author object
-  function getAuthor(book, authors) {
-    const author = authors.find((author) => author.id === book.authorId);
-    return author;
-  }
+  });
+  let result = borrowedBooks.map((book) => {
+    return { ...book, author: _getAuthor(book, authors) };
+  });
+  return result;
+}
 
 module.exports = {
   findAccountById,
